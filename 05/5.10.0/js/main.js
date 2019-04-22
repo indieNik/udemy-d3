e3915ca67e5e8ba34402d5292b8ca862a2e705ca
Nikhil Patil <nikhil@engagedly.com>
2019-04-22 18:52:48 +0530

    Gapminder Clone [WIP]

--- 05/5.10.0/js/main.js
+++ 05/5.10.0/js/main.js
@@ -4,6 +4,22 @@
 *    Project 2 - Gapminder Clone
 */
 
+
+var f_data = [];
+
 d3.json("data/data.json").then(function(data){
-	console.log(data);
-})
+	data.forEach(d => {
+		f_data = d.countries.filter(c => c.income != null && c.life_exp != null)
+	});
+});
+
+var margin = { top: 10, right: 10, bottom: 100, left: 100 },
+	width = 600 - margin.left - margin.right,
+	height = 400 - margin.top - margin.bottom;
+
+var g = d3.select('#chart-area').append('svg')
+	.attr('width', width + margin.left + margin.right)
+	.attr('height', height + margin.top + margin.bottom)
+		.append('g')
+			.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
